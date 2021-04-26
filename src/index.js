9faa9407683ef61f309e581783dd2f65fcfaef58
Jorge Núñez Santiago <jorge.nusan@gmail.com>
2021-04-26 20:58:23 +0200

    Nusan icon added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 $('#navbar').load('../components/navbar.html')
-
+/*
 document.querySelector('#btnLogin').addEventListener('click', guardarDatosLogin());
 
 function guardarDatosLogin(){
@@ -12,4 +12,5 @@ function guardarDatosLogin(){
     }
 
     console.log(login);
-}
+}*/
+
