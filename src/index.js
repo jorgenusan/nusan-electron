251eadc202839d256abb766ca4c69c3b9bd688e4
Jorge Núñez Santiago <jorge.nusan@gmail.com>
2021-04-26 11:58:16 +0200

    Trying new function for login

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 $('#navbar').load('../components/navbar.html')
+
+document.querySelector('#btnLogin').addEventListener('click', guardarDatosLogin());
+
+function guardarDatosLogin(){
+    let guardarEmail = document.getElementById('InputEmail').nodeValue;
+    let guardarPassword = document.getElementById('InputPassword').nodeValue;
+
+    var login = {
+        email : guardarEmail,
+        password : guardarPassword
+    }
+
+    console.log(login);
+}
