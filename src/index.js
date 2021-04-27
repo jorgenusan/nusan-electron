9d320101d4395351488479aee481026fcf296b26
Jorge Núñez Santiago <jorge.nusan@gmail.com>
2021-04-27 20:35:15 +0200

    error in the ajax call of the login

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
 $('#navbar').load('../components/navbar.html')
-/*
-document.querySelector('#btnLogin').addEventListener('click', guardarDatosLogin());
 
-function guardarDatosLogin(){
-    let guardarEmail = document.getElementById('InputEmail').nodeValue;
-    let guardarPassword = document.getElementById('InputPassword').nodeValue;
+function confirmLogin(){
+    let urlString = 'http://localhost:8080/login';
+    let email =$('#InputEmail').val();
+    let password = $('#InputPassword').val()
 
-    var login = {
-        email : guardarEmail,
-        password : guardarPassword
-    }
+    console.log(email);
+    console.log(password);
 
-    console.log(login);
-}*/
+    var datajson = JSON.stringify({
+        "email":email,
+        "password":password
+    })
 
+    console.log(datajson);
+
+     $.ajax({ 
+        type: "POST",
+        url:urlString,
+        crossDomain:true,
+        contentType: 'application/json; charset=utf-8',
+        data:datajson
+    })
+        .done(function(){
+            console.log("success");
+           // window.location.replace(index.html);
+        })
+        .fail(function() {
+            console.log("Email o contraseña incorrecto.");
+        })
+  }
