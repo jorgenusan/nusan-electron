7c4b777dc27edfeb73da46ce80430da1bb06b42b
Jorge Núñez Santiago <jorge.nusan@gmail.com>
2021-04-28 22:26:02 +0200

    index.js fixed. New content.js

---
+++ src/js/index.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $("form").submit(function(event){
+        var formData = JSON.stringify({
+            email: $("#InputEmail").val(),
+            password: $("#InputPassword").val()
+        });
+        $.ajax({
+            type:"POST",
+            url: "http://localhost:8080/login",
+            data: formData,
+            contentType: "application/json"
+        }).done(function(data){
+            console.log("done");
+            window.location.replace("./index.html");
+        }).fail(function(error){
+            console.log("error");
+            alert("Email o contraseña incorrecto.")
+        })
+      event.preventDefault();
+    });
+});
