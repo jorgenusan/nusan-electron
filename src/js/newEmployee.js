769800a15fdf003f7c56eb53caad9f61c9822b90
Jorge Núñez Santiago <jorge.nusan@gmail.com>
2021-05-05 22:48:40 +0200

    toggle password help

--- src/js/newEmployee.js
+++ src/js/newEmployee.js
@@ -1,30 +1,40 @@
 
-    function saveEmp(event){
-        password= $('#inputPassword').val();
-        password2= $('#inputPassword2').val();
-        if(password == password2){
-            var formData = JSON.stringify({
-                name: $("#inputName").val(),
-                lastName: $("#inputLastName").val(),
-                dni: $('#inputDni').val(),
-                email: $('#inputEmail').val(),
-                phoneNumber: $('#inputTel').val(),
-                password: $('#inputPassword').val()
-            });
+function saveEmp(event){
+    password= $('#inputPassword').val();
+    password2= $('#inputPassword2').val();
+    if(password == password2){
+        var formData = JSON.stringify({
+            name: $("#inputName").val(),
+            lastName: $("#inputLastName").val(),
+            dni: $('#inputDni').val(),
+            email: $('#inputEmail').val(),
+            phoneNumber: $('#inputTel').val(),
+            password: $('#inputPassword').val()
+        });
     
-            $.ajax({
-                type:"POST",
-                url: "http://localhost:8080/employees",
-                data: formData,
-                contentType: "application/json"
-            }).done(function(data){
-                alert("El empleado ha sido creado correctamente.");
-            }).fail(function(error){
-                alert("Error al crear el empleado.")
-            })
-        }else{
-            alert("Las contraseñas no coindicen.");
-        }
-       
-      event.preventDefault();
+        $.ajax({
+            type:"POST",
+            url: "http://localhost:8080/employees",
+            data: formData,
+            contentType: "application/json"
+        }).done(function(data){
+            alert("El empleado ha sido creado correctamente.");
+        }).fail(function(error){
+            alert("Error al crear el empleado.")
+        })
+    }else{
+        alert("Las contraseñas no coindicen.");
     }
+       
+    event.preventDefault();
+}
+
+function help(){
+    var x = document.getElementById("passwordHelp");
+    if(x.innerHTML === ""){
+        x.innerHTML = "Debe contener una mayúscula, un número, un caracter especiál y ser mayor de 6 caracteres.";
+    } else {
+        x.innerHTML = "";
+    }
+};
+    
